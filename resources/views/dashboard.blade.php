478273479575418bf6e84f4319b05e3d3f7ef806
adith-widhiantara <172410101108@cs.unej.ac.id>
2021-03-09 17:34:54 +0700

    Update Permission

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,28 @@
             </div>
         </div>
     </div>
+
+    @can('create news')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    permision create news
+                </div>
+            </div>
+        </div>
+    </div>
+    @endcan
+
+    @role('employee')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    employee role
+                </div>
+            </div>
+        </div>
+    </div>
+    @endrole
 </x-app-layout>
